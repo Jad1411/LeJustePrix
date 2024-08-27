2565d18f444dab4d3a84561e9ca767225e18500d
hallanic-isep <jad.farina@eleve.isep.fr>
2024-08-27 13:54:03 +0200

    ajout du fichier python

---
+++ LeJustePrix.py
@@ -0,0 +1,19 @@
+import random
+b = random.randint(1, 10)
+n = input("Comment t'appelles tu?")
+a = int(input("Devinez un prix entre 1 et 100."))
+for k in range(1, 5):
+    if a < b:
+        print("C'est +")
+        print("Nombre d'essai restants:", 5 - k)
+        a = int(input("réessaye"))
+    elif a > b:
+        print("C'est -")
+        print("Nombre d'essai restants:", 5 - k)
+        a = int(input("réessaye"))
+    elif a == b:
+        print("C'est JUSTE!!!!!!!")
+        print("BRAVO")
+        exit()
+print(f"{n},c'est raté")
+exit()
