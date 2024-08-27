0208d1f85bfe529a0b5cb5b372d978578faa498a
hallanic-isep <jad.farina@eleve.isep.fr>
2024-08-27 14:57:59 +0200

    Résolution d'erreur

--- LeJustePrix.py
+++ LeJustePrix.py
@@ -1,7 +1,7 @@
 import random
 b = random.randint(1, 10)
 n = input("Comment t'appelles tu?")
-a = int(input("Devinez un prix entre 1 et 100."))
+a = int(input("Devinez un prix entre 1 et 10."))
 for k in range(1, 5):
     if a < b:
         print("C'est +")
